9e49dd3d7711a552f0510302224d3887eb1f592f
mahesh9Cs <maheshmumbai@gmail.com>
2025-01-22 15:00:49 +0530

    Shell-Scripts

--- Shell-Scripting/05-variable.sh
+++ Shell-Scripting/05-variable.sh
@@ -4,4 +4,5 @@ echo "Please enter username:"
 read -s USERNAME
 echo " Please enter password:"
 read -s PASSWORD
-echo "Username is $USERNAME Password is $PASSWORD"
+echo "Username = $USERNAME
+      Password = $PASSWORD"
