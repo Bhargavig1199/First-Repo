905add37a628769a5932cc6aa3bcbd1b4eb299db
mahesh9Cs <maheshmumbai@gmail.com>
2025-01-22 14:01:21 +0530

    Shell-Scripts

--- Shell-Scripting/02-variable.sh
+++ Shell-Scripting/02-variable.sh
@@ -1,4 +1,8 @@
-echo "Ramesh : Hello Suresh, how are you?"
-echo "Suresh : Hi Ramesh, I am good how are you?"
-echo "Ramesh : I need to connect you on one of the business deal"
-echo "Suresh : Yeah Ramesh you can reach me on PH : XXXXXXXXXX"
+#!/bin/bash
+
+PERSON1=$(Ramesh)
+PERSON2=$(Suresh)
+echo "$PERSON1 : Hello $PERSON2, how are you?"
+echo "$PERSON2 : Hi $PERSON2, I am good how are you?"
+echo "$PERSON1 : I need to connect you $PERSON2"
+echo "$PERSON2 : Yeah $PERSON1 you can reach me on PH : XXXXXXXXXX"
