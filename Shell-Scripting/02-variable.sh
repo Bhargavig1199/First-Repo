f6d9b0388fd68cef90cc8536bc995bab4c6aabf4
mahesh9Cs <maheshmumbai@gmail.com>
2025-01-22 13:38:17 +0530

    Shell-Scripts

--- Shell-Scripting/02-variable.sh
+++ Shell-Scripting/02-variable.sh
@@ -1,4 +1,4 @@
 echo "Ramesh : Hello Suresh, how are you?"
 echo "Suresh : Hi Ramesh, I am good how are you?"
 echo "Ramesh : I need to connect you on one of the business deal"
-echo ":Suresh : Yeah Ramesh you can reach me on PH : XXXXXXXXXX"
+echo "Suresh : Yeah Ramesh you can reach me on PH : XXXXXXXXXX"
